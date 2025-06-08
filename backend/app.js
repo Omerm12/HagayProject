2c6e09ba14c5696650d8edefd17e4156ad06591f
Omer Madhala <omermadhala181@gmail.com>
2025-06-08 19:52:06 +0300

    Update files

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+import pool from './db.js';
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
