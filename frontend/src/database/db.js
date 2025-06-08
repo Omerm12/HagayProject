7ea812e9779b195e962f482c5c595dba6add4230
Omer Madhala <omermadhala181@gmail.com>
2025-06-08 19:22:49 +0300

    Update files

--- frontend/src/database/db.js
+++ frontend/src/database/db.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL + '?ssl=true',
+  host: process.env.DB_HOST,  // dpg-d128j5re5dus73f2dlg0-a.frankfurt-postgres.render.com
+  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER,  // veggie_shop_user
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, // veggie_shop
   ssl: {
     rejectUnauthorized: false
   }
