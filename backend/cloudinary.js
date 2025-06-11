d6ef9bd78e2809574258c6b9d4ffeabd850d6583
Omer Madhala <omermadhala181@gmail.com>
2025-06-11 23:38:17 +0300

    Update files

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,9 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: 'dvwmz4xmy',
-  api_key: '718284864537457',
-  api_secret: 'l1VBFRV_nM8WOEc3bCjpY3PLQEs',
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 export default cloudinary;
